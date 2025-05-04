Test broadcasting and fix some bugs.

src/gettsim_personas/broadcasting.py
@@ -22,26 +22,27 @@ def broadcast_p_id(original_series: pd.Series, expected_length: int) -> pd.Serie
     return pd.Series(list(range(expected_length)))
 
 
-def broadcast_series_with_group_or_foreign_keys(
-    original_series: pd.Series, expected_length: int
-) -> pd.Series:
-    """Broadcast series with group or foreign keys.
+def broadcast_group_ids(original_series: pd.Series, expected_length: int) -> pd.Series:
+    """Broadcast series with group IDs.
+
+    Group IDs are used to identify groups of rows that should be treated together.
+    The exact values don't matter as long as they maintain the same grouping pattern.
 
     Example:
     -------
-    >>> original_series = pd.Series([0, 1, 2])
+    >>> original_series = pd.Series([0, 1, 1])
     >>> expected_length = 6
-    >>> broadcast_series_with_group_or_foreign_keys(original_series, expected_length)
+    >>> broadcast_group_ids(original_series, expected_length)
     >>> 0    0
     >>> 1    1
-    >>> 2    2
-    >>> 3    3
-    >>> 4    4
-    >>> 5    5
+    >>> 2    1
+    >>> 3    2
+    >>> 4    2
+    >>> 5    3
 
     >>> original_series = pd.Series([-1, 0, 2])
     >>> expected_length = 6
-    >>> broadcast_series_with_group_or_foreign_keys(original_series, expected_length)
+    >>> broadcast_group_ids(original_series, expected_length)
     >>> 0    -1
     >>> 1    0
     >>> 2    2
@@ -51,11 +52,53 @@ def broadcast_series_with_group_or_foreign_keys(
     """
     number_of_personas = expected_length // len(original_series)
 
-    repeated_series = pd.Series(np.tile(original_series.values, number_of_personas))
+    repeated_series = pd.Series(
+        np.tile(original_series.values, reps=number_of_personas)
+    )
 
-    # How much to add to each row of the repeated series (if it is not -1)
+    original_ids = original_series[original_series >= 0].unique()
+
+    max_original_id = max(original_ids) if len(original_ids) > 0 else 0
     to_add_if_not_minus_one = np.repeat(
-        np.arange(number_of_personas) * len(original_series), len(original_series)
+        np.arange(number_of_personas) * (max_original_id + 1),
+        repeats=len(original_series),
+    )
+
+    is_valid_id = repeated_series >= 0
+    repeated_series[is_valid_id] += to_add_if_not_minus_one[is_valid_id]
+
+    return repeated_series
+
+
+def broadcast_foreign_keys(
+    original_series: pd.Series, expected_length: int
+) -> pd.Series:
+    """Broadcast series with foreign keys.
+
+    Foreign keys are used to reference specific rows in another table.
+    The values must be preserved exactly to maintain the correct relationships.
+
+    Example:
+    -------
+    >>> original_series = pd.Series([1, 0, -1])
+    >>> expected_length = 6
+    >>> broadcast_foreign_keys(original_series, expected_length)
+    >>> 0    1
+    >>> 1    0
+    >>> 2    -1
+    >>> 3    4
+    >>> 4    3
+    >>> 5    -1
+    """
+    number_of_personas = expected_length // len(original_series)
+
+    repeated_series = pd.Series(
+        np.tile(original_series.values, reps=number_of_personas)
+    )
+
+    to_add_if_not_minus_one = np.repeat(
+        np.arange(number_of_personas) * len(original_series),
+        repeats=len(original_series),
     )
 
     is_valid_id = repeated_series >= 0
@@ -65,11 +108,11 @@ def broadcast_series_with_group_or_foreign_keys(
 
 
 def _fail_if_persona_p_id_invalid(p_id_series: pd.Series) -> None:
-    """Fail if persona p_id does not start with 0 and increment in steps of 1."""
+    """Fail if persona p_id does not start with 0 or increment in steps of 1."""
     valid = pd.Series(list(range(len(p_id_series))))
     if not p_id_series.equals(valid):
         msg = f"""
-        Persona p_id does not start with 0 and increment in steps of 1.
+        Persona p_id does not start with 0 or increment in steps of 1.
         Found: {p_id_series.to_list()}
         """
         raise ValueError(msg)

src/gettsim_personas/interface.py
@@ -12,8 +12,9 @@ import pandas as pd
 from gettsim_personas.broadcasting import (
     _fail_if_data_lengths_are_incompatible,
     _fail_if_data_to_upsert_is_not_dict_with_series_leafs,
+    broadcast_foreign_keys,
+    broadcast_group_ids,
     broadcast_p_id,
-    broadcast_series_with_group_or_foreign_keys,
 )
 from gettsim_personas.load_personas import load_personas
 from gettsim_personas.persona_objects import PersonaCollection
@@ -87,8 +88,13 @@ def upsert_input_data(
                 original_series=series,
                 expected_length=expected_length,
             )
-        elif "p_id_" in name or name.endswith("_id"):
-            broadcasted_series = broadcast_series_with_group_or_foreign_keys(
+        elif "p_id_" in name:
+            broadcasted_series = broadcast_foreign_keys(
+                original_series=series,
+                expected_length=expected_length,
+            )
+        elif name.endswith("_id"):
+            broadcasted_series = broadcast_group_ids(
                 original_series=series, expected_length=expected_length
             )
         else:
@@ -96,8 +102,8 @@ def upsert_input_data(
                 np.tile(series.values, number_of_new_personas)
             )
 
-        upserted_data.name = dt.tree_path_from_qual_name(name)[-1]
-        upserted_data.index = pd.RangeIndex(expected_length)
+        broadcasted_series.name = dt.tree_path_from_qual_name(name)[-1]
+        broadcasted_series.index = pd.RangeIndex(expected_length)
         upserted_data[name] = broadcasted_series
 
     return dt.unflatten_from_qual_names(upserted_data)