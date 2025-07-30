351379cf943c3da43f8f9181644f5ff0e11bb3f5
Marvin Immesberger <immesberger@posteo.de>
2025-07-30 19:06:45 +0200

    Expose upserting function.

--- src/gettsim_personas/__init__.py
+++ src/gettsim_personas/__init__.py
@@ -1,7 +1,8 @@
 """Personas to use as example data when calling GETTSIM."""
 
 from gettsim_personas.persona_objects import _GETTSIMPersonas
+from gettsim_personas.upsert import upsert_input_data
 
 GETTSIMPersonas = _GETTSIMPersonas()
 
-__all__ = ["GETTSIMPersonas"]
+__all__ = ["GETTSIMPersonas", "upsert_input_data"]
