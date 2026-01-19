41ba3f8420197f5a87a0afae2a9d81ece684b513
Marvin Immesberger <immesberger@posteo.de>
2026-01-19 20:29:57 +0100

    Expose Single persona.

--- src/gettsim_personas/grundsicherung_für_erwerbsfähige/__init__.py
+++ src/gettsim_personas/grundsicherung_für_erwerbsfähige/__init__.py
@@ -1,6 +1,11 @@
 from _gettsim_personas.de.grundsicherung_für_erwerbsfähige import (
     Couple1Child,
     Couple1ChildInKarenzzeit,
+    SingleAdult,
 )
 
-__all__ = ["Couple1Child", "Couple1ChildInKarenzzeit"]
+__all__ = [
+    "Couple1Child",
+    "Couple1ChildInKarenzzeit",
+    "SingleAdult",
+]
